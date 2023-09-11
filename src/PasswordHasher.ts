55d7cc1a9a129fe66d4f771d7da45d5a9c290b98
Souhail Krissaane <61523279+SouhailKrs@users.noreply.github.com>
2023-09-11 16:59:10 +0100

    Update PasswordHasher.ts

--- src/PasswordHasher.ts
+++ src/PasswordHasher.ts
@@ -1,3 +1,10 @@
+/**
+ * Hash a password using the built-in password hashing in Bun.js.
+ *
+ * @async
+ * @param {string} password - The password to be hashed.
+ * @returns {Promise<string>} A hashed password string.
+ */
 export const hashPassword = async (password: string) => {
     return await Bun.password.hash(password);
 };
